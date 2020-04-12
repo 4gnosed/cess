d2b5b5cba54ea4fda42f1aba6f9db4a0e7def8c1
4gnosed <gnosed@foxmail.com>
2020-04-12 23:54:06 +0800

    企业信息更新

---
+++ src/main/java/cn/edu/cess/controller/content/enterprise/EnterpriseController.java
@@ -0,0 +1,65 @@
+package cn.edu.cess.controller.content.enterprise;
+
+
+import cn.edu.cess.base.AbstractClass;
+import cn.edu.cess.entity.content.enterprise.Enterprise;
+import cn.edu.cess.entity.content.enterprise.UserEnterprise;
+import cn.edu.cess.result.Result;
+import cn.edu.cess.result.ResultFactory;
+import cn.edu.cess.service.content.enterprise.IEnterpriseService;
+import cn.edu.cess.service.content.enterprise.IUserEnterpriseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author Gnosed Lu
+ * @since 2020-04-12
+ */
+@RestController
+@RequestMapping("/api/enterprise")
+public class EnterpriseController extends AbstractClass {
+
+    @Autowired
+    IUserEnterpriseService iUserEnterpriseService;
+
+    @Autowired
+    IEnterpriseService iEnterpriseService;
+
+    @GetMapping("/getOne")
+    public Result getOneEnterprise(@RequestParam("userId") Integer userId) {
+        //根据用户角色绑定对应的角色对象
+        UserEnterprise userEnterprise = iUserEnterpriseService.listByUid(userId);
+        if (userEnterprise == null) {
+            //未填写信息
+            return ResultFactory.buildEmptyResult("");
+        } else if (userEnterprise.getEnabled() == false) {
+            //未审核通过
+            return ResultFactory.buildFailResult("");
+        }
+        Enterprise Enterprise = iEnterpriseService.getById(userEnterprise.getEid());
+        iEnterpriseService.fillData(Enterprise);
+        return ResultFactory.buildSuccessResult(Enterprise);
+    }
+
+    @PutMapping("")
+    public Result editEnterprise(@RequestBody Enterprise enterprise) {
+        iEnterpriseService.updateEnterprise(enterprise);
+        return ResultFactory.buildSuccessResult("");
+    }
+
+    @PostMapping("")
+    public Result addEnterprise(@RequestBody Enterprise enterprise, @RequestParam("userId") Integer userId) {
+        enterprise.setId(iEnterpriseService.getLastId() + 1);
+        UserEnterprise userEnterprise = new UserEnterprise();
+        userEnterprise.setUid(userId);
+        userEnterprise.setEid(enterprise.getId());
+        userEnterprise.setEnabled(false);
+        iUserEnterpriseService.save(userEnterprise);
+        iEnterpriseService.save(enterprise);
+        return ResultFactory.buildSuccessResult(enterprise);
+    }
+}
