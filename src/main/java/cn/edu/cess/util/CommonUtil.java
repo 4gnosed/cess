381271a3a462905375bd5f175aa0842d139465a7
gnosed <gnosed@foxmail.com>
2021-06-01 18:24:10 +0800

    弱密码匹配

--- src/main/java/cn/edu/cess/util/CommonUtil.java
+++ src/main/java/cn/edu/cess/util/CommonUtil.java
@@ -29,4 +29,36 @@ public class CommonUtil {
         }
         return ResultFactory.buildFailResult(s.toString());
     }
+
+    /**
+     * 匹配：
+     * 第一种情况：连续字符（ascci字典序）
+     * 第二种情况：所有字符相同
+     *
+     * @param str
+     * @return 只要满足一种情况，返回true，否则返回false
+     */
+    public static boolean matchStr(String str) {
+        if (str == null || StringUtil.isEmpty(str) || str.length() == 1) {
+            return false;
+        }
+        if (str.length() == 2) {
+            return str.charAt(0) + 1 == str.charAt(1) || str.charAt(0) == str.charAt(1);
+        }
+        boolean continueStr = true;
+        boolean equalsStr = true;
+        for (int i = 0; i < str.length() - 1; i++) {
+            char c = str.charAt(i);
+            char next = str.charAt(i + 1);
+            if (continueStr && c + 1 != next) {
+                continueStr = false;
+            }
+            if (equalsStr && c != next) {
+                equalsStr = false;
+            }
+        }
+        return continueStr || equalsStr;
+    }
+
+
 }
---
+++ src/test/java/JustTest.java
@@ -0,0 +1,17 @@
+import cn.edu.cess.util.CommonUtil;
+
+/**
+ * @Package: PACKAGE_NAME
+ * @Description:
+ * @Author: LuDeSong
+ * @Date: 2021-6-1 16:23
+ */
+
+public class JustTest {
+
+    public static void main(String[] args) {
+        String s="1234566";
+        boolean b = CommonUtil.matchStr(s);
+        System.out.println(b);
+    }
+}
