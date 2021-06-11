08814d0c884ff08f8ef28331c95b2e9faa852009
gnosed <gnosed@foxmail.com>
2021-06-11 14:38:17 +0800

    log

--- src/main/java/cn/edu/cess/config/GlobalExceptionHandler.java
+++ src/main/java/cn/edu/cess/config/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import cn.edu.cess.exception.CustomException;
 import cn.edu.cess.result.Result;
 import cn.edu.cess.result.ResultCode;
 import cn.edu.cess.result.ResultFactory;
+import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,7 +19,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public Result handleOtherExceptions(Exception e) {
-        log.info("Exception e:{}", e);
+        log.info("Exception e:{}", e.getMessage());
         if (e instanceof CustomException) {
             return ResultFactory.buildResult(ResultCode.FAIL, ((CustomException) e).getMessageContent(), "");
         } else {
