b9432ee1334d54324b3baa10dbf2f3d2b19bfa1e
gnosed <gnosed@foxmail.com>
2021-06-24 17:53:29 +0800

    查询读从库   @DataSourceType

--- src/main/java/cn/edu/cess/service/content/student/impl/StudentServiceImpl.java
+++ src/main/java/cn/edu/cess/service/content/student/impl/StudentServiceImpl.java
@@ -1,7 +1,7 @@
 package cn.edu.cess.service.content.student.impl;
 
 import cn.edu.cess.config.datasource.druid.DruidConfigCluster;
-import cn.edu.cess.config.datasource.dynamic.DataSource;
+import cn.edu.cess.config.datasource.dynamic.DataSourceType;
 import cn.edu.cess.constant.Constant;
 import cn.edu.cess.entity.content.student.*;
 import cn.edu.cess.mapper.content.student.StudentMapper;
@@ -54,7 +54,7 @@ public class StudentServiceImpl extends ServiceImpl<StudentMapper, Student> impl
      * @return
      */
     @Override
-    @DataSource(DruidConfigCluster.CLUSTER_DATA_SOURCE)
+    @DataSourceType(DruidConfigCluster.CLUSTER_DATA_SOURCE)
     public ResultPage getStudentByPage(Integer page, Integer size, Student student, String[] beginDateScope) {
         QueryWrapper<Student> queryWrapper = new QueryWrapper<>();
         String name = student.getName();
